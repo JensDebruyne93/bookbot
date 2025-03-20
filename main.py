9c738adc77991af4643bab1742abe3cb539ebafb
JensDebruyne93 <jens.debruyne@live.be>
2025-03-21 00:39:14 +0100

    add books

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from stats import get_num_words, count_characters, print_report
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    filepath = "books/frankenstein.txt"
-    text = get_book_text(filepath)
-    wordCount = get_num_words(text)
-    characters = count_characters(text)
-    print(print_report(filepath,wordCount,characters))
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        filepath = sys.argv[1]
+        text = get_book_text(filepath)
+        wordCount = get_num_words(text)
+        characters = count_characters(text)
+        print(print_report(filepath,wordCount,characters))
 main()
